b9d48dcd5d316254589405e61eb4cf29a2bef484
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-02-12 14:43:35 +0100

    restaurant and reviews works well, i have to add css now

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
+  NUMB = [0, 1, 2, 3, 4, 5].freeze
   belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: NUMB }
+  validates :rating, numericality: { only_integer: true }
 end
--- db/migrate/20200212105419_create_restaurants.rb
+++ db/migrate/20200212105419_create_restaurants.rb
@@ -2,7 +2,7 @@ class CreateRestaurants < ActiveRecord::Migration[6.0]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.text :adress
+      t.text :address
       t.string :phone_number
       t.string :category
 
